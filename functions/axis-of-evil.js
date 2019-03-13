bbd46914f733d5cc9b70a3d1983e7ffabb6fe883
Mikko Forsström <pekkisx@gmail.com>
2019-03-13 12:22:59 +0200

    Foobar.

--- functions/axis-of-evil.js
+++ functions/axis-of-evil.js
@@ -1,6 +1,12 @@
 exports.handler = async (event, context, callback) => {
   return {
     statusCode: 200,
-    body: "Hello, World",
+    body: JSON.stringify([
+      { name: "Abu Pekkis" },
+      { name: "Adolf Hitler" },
+      { name: "Kim Jong-Un" },
+      { name: "Saddam Hussein" },
+      { name: "Bashar al-Assad" },
+    ]),
   }
 }
